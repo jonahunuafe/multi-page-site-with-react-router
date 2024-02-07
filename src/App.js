57ffb54c212f098df20a376b294d16cacf6cf4b4
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-07 09:40:03 +0100

    adding the RootLayout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home"
 import ProductPage from "./pages/Product";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path: "/", element: <HomePage />},
-  {path: "/products", element: <ProductPage />}
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {path: "/", element: <HomePage />},
+      {path: "/products", element: <ProductPage />}
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
